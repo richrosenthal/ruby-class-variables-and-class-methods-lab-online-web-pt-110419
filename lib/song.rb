0be8be1842d0abbf3d827b7b5069221651a87bbe
Richard Rosenthal <richard.rosenthal@acd.ccac.edu>
2019-12-22 15:21:36 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -1,4 +1,12 @@
 class Song 
   
   attr_accessor :name, :artist, :genre
-  def initialize
+  
+  def initialize(name, artist, genre)
+    @name = name
+    @artist = artist
+    @genre = genre
+  end
+  
+end
+    
