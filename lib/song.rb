8c1015cf1ccd0da8d72b135aa15fa64fd5b583c5
Richard Rosenthal <richard.rosenthal@acd.ccac.edu>
2019-12-22 16:37:55 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -3,28 +3,30 @@ class Song
   attr_accessor :name, :artist, :genre
   @@count = 0 
   @@genres = []
-  
+  @@artists = []
   def initialize(name, artist, genre)
     @name = name
     @artist = artist
     @genre = genre
     
+    
     @@count += 1
     @@genres << @genres
+    @@artists << @artist
   end
   def self.count
     @@count
   end 
   
   def genres 
-    unique_genres = []
-    @@genres.each do |genres| 
-      if !@@genres.include?(genres)
-        unique_genres << genres
-      end
+  #   unique_genres = []
+  #   @@genres.each do |genres| 
+  #     if !@@genres.include?(genres)
+  #       unique_genres << genres
+  #     end
     
-  end
-  unique_genres
+  # end
+  # unique_genres
 end
 end
     
